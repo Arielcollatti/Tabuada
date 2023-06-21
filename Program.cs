952283ef12a0b2e3c43a736a2aeddda7d911694c
arielcollatti <colatti2017@gmail.com>
2023-06-21 15:49:01 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine(" ---Tabuada--- \n");
+
+Console.WriteLine("Inicio:");
+
+Console.Write("Digite um Valor para a Tabuada...: ");
+int numero = int.Parse(Console.ReadLine()!);
+
+int contador = 1;
+
+while (contador <= 10)
+{
+   int resultado = numero * contador;
+    Console.WriteLine($"{numero} x {contador} = {resultado}");
+    contador = contador +1;
+}
+
+ Console.WriteLine("  Fim. \n");
